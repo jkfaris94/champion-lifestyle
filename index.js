833e5d66bf260b48f746cd40c4161e16ff2fe337
Johnny Faris <jkfaris@gmail.com>
2024-11-19 16:36:09 -0800

    working properly, need to add alerts of message sent in contact section, also need to adjust calorie counter to adjust based on goal

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function sendMail(){
-  let parms = {
+  let params = {
       name : document.getElementById("name").value,
       email : document.getElementById("email").value,
       message : document.getElementById("message").value,
   }
 
-  emailjs.send("service_t458qp2", "template_hjmej7w",parms).then(alert("Your Message has sent!"))
+  emailjs.send("service_t458qp2", "template_hjmej7w",params).then(alert("Your Message has sent!"))
 }

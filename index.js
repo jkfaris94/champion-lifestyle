8154ef7186bf17be0539497d457ddb1514ba0430
Johnny Faris <jkfaris@gmail.com>
2024-11-22 15:12:04 -0800

    added readMore() to hide and show text. touched css index.html and index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function sendMail(){
+function sendMail() {
   let params = {
       name : document.getElementById("name").value,
       email : document.getElementById("email").value,
@@ -6,4 +6,17 @@ function sendMail(){
   }
 
   emailjs.send("service_t458qp2", "template_hjmej7w",params).then(alert("Your Message has sent!"))
+}
+
+function readMore () {
+  const moreText = document.getElementById("moreText");
+  const readMore = document.getElementById("readMore");
+
+  if (moreText.classList.contains("hidden")) {
+      moreText.classList.remove("hidden");
+      readMore.textContent = "Read Less";
+  } else {
+    moreText.classList.add("hidden");
+    readMore.textContent = "..."
+  }
 }

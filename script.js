aae46c3b63faae4d9a81c503ad7e1304f5b87eb3
Johnny Faris <jkfaris@gmail.com>
2024-11-26 17:29:18 -0800

    added event listener for readMore button. also improved readmore toggle

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const readMoreButton = document.getElementById("readMore");
+  if (readMoreButton) {
+      readMoreButton.addEventListener("click", toggleReadMore);
+  }
+});
+
 function sendMail() {
   let params = {
       name : document.getElementById("name").value,
@@ -8,15 +15,17 @@ function sendMail() {
   emailjs.send("service_t458qp2", "template_hjmej7w",params).then(alert("Your Message has sent!"))
 }
 
-function readMore () {
+function toggleReadMore() {
   const moreText = document.getElementById("moreText");
   const readMore = document.getElementById("readMore");
 
-  if (moreText.classList.contains("hidden")) {
-      moreText.classList.remove("hidden");
-      readMore.textContent = "Read Less";
-  } else {
-    moreText.classList.add("hidden");
-    readMore.textContent = "..."
+  if (moreText && readMore) {
+      if (moreText.classList.contains("hidden")) {
+          moreText.classList.remove("hidden");
+          readMore.textContent = "Read Less";
+      } else {
+          moreText.classList.add("hidden");
+          readMore.textContent = "Read More";
+      }
   }
 }
